c606ceed3efa3c5748d30fabd7b7d7eabada8469
おちラボ <yochi@ochi-lab.org>
2026-01-02 18:47:42 +0900

    Add header comment for video blink detection script
    Added a detailed header comment explaining the purpose and functionality of the video blink detection program.

--- CV/video_blink_detection.py
+++ CV/video_blink_detection.py
@@ -1,3 +1,34 @@
+"""
+Video Blink Detection - 動画から瞬き検出プログラム
+
+このプログラムは動画ファイルから人物の瞬きを自動検出し、詳細なログとレポートを生成します。
+MediaPipe Face Landmarkerを使用して顔のランドマークを検出し、
+目のアスペクト比（EAR）を計算することで瞬きを判定します。
+
+Author: OchiLab
+Version: 1.0.0
+License: MIT License
+copyright (c) 2024 OchiLab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
